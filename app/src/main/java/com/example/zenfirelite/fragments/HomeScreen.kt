19bb21c51892d0034d5af0ad1e6ae64d76af2a00
Neel Patel <neelpatel29072001>
2024-05-01 03:40:00 +0530

    set calender

--- app/src/main/java/com/example/zenfirelite/fragments/HomeScreen.kt
+++ app/src/main/java/com/example/zenfirelite/fragments/HomeScreen.kt
@@ -1,14 +1,21 @@
 package com.example.zenfirelite.fragments
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.example.zenfirelite.R
+import com.example.zenfirelite.databinding.FragmentHomeScreenBinding
+import java.text.DateFormatSymbols
+import java.util.Calendar
 
 
 class HomeScreen : Fragment() {
+
+    private lateinit var binding: FragmentHomeScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +28,41 @@ class HomeScreen : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_screen, container, false)
+        binding = FragmentHomeScreenBinding.inflate(inflater, container, false)
+        binding.startDate.setOnClickListener {
+            openCalenderPicker(binding.startDate)
+        }
+        binding.toDate.setOnClickListener {
+            openCalenderPicker(binding.toDate)
+        }
+        return binding.root
     }
+
+    private fun openCalenderPicker(dateValue: EditText) {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = context?.let { it1 ->
+            DatePickerDialog(
+                it1,
+                R.style.MyDatePickerDialogStyle,
+                { view, year, monthOfYear, dayOfMonth ->
+                    // Get the month name from the month number
+                    val monthName = DateFormatSymbols().shortMonths[monthOfYear]
+                    val date = "$dayOfMonth-$monthName-$year"
+                    dateValue.setText(date)
+                },
+                year,
+                month,
+                day
+            )
+        }
+        datePickerDialog?.show()
+    }
+
+
 
 
 }
