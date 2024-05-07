cbe44c69d2e2f3e857f822db590fc4f0fb477601
Neel Patel <neelpatel29072001>
2024-05-07 18:06:27 +0530

    set fireInspector dropdown_searchable

--- app/src/main/java/com/example/zenfirelite/adapters/AdapterForFireInspectorList.kt
+++ app/src/main/java/com/example/zenfirelite/adapters/AdapterForFireInspectorList.kt
@@ -1,13 +1,22 @@
 package com.example.zenfirelite.adapters
 
+import android.app.Dialog
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.zenfirelite.R
+import com.example.zenfirelite.fragments.InspectionInfo
 
-class AdapterForFireInspectorList(private val inspectorName : List<String>):
+class AdapterForFireInspectorList(
+    private val inspectorName : List<String>,
+    private val setFireInspector : TextView,
+    private val dialog: Dialog
+):
       RecyclerView.Adapter<AdapterForFireInspectorList.ViewHolder>(){
 
 
@@ -29,6 +38,11 @@ class AdapterForFireInspectorList(private val inspectorName : List<String>):
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val ItemsViewModel = inspectorName[position]
         holder.inspectorName.text = ItemsViewModel
+
+        holder.itemView.setOnClickListener {
+            setFireInspector.hint = ItemsViewModel
+            dialog.dismiss()
+        }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/zenfirelite/fragments/InspectionInfo.kt
+++ app/src/main/java/com/example/zenfirelite/fragments/InspectionInfo.kt
@@ -1,16 +1,23 @@
 package com.example.zenfirelite.fragments
 
+import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.EditText
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -21,6 +28,7 @@ import com.example.zenfirelite.R
 import com.example.zenfirelite.adapters.AdapterForFireInspectorList
 import com.example.zenfirelite.databinding.FragmentInspectionInfoBinding
 import com.example.zenfirelite.datamodels.InspectionInfoModel
+import java.util.Locale
 
 class InspectionInfo : Fragment() {
 
@@ -64,24 +72,67 @@ class InspectionInfo : Fragment() {
 
         val fireInspectorList = ArrayList<String>()
 
-        for (i in 1..50) {
+        for (i in 1..10) {
             fireInspectorList.add("Neel Patel")
+            fireInspectorList.add("Smit Patel")
+            fireInspectorList.add("Kuldeep Tripathi")
         }
 
-        binding.fireInspector.setOnClickListener{
+        binding.fireInspector.setOnClickListener {
+            val dialog = Dialog(requireContext())
+            dialog.setContentView(R.layout.fireinspector_dialogspinner)
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            dialog.window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
 
-                val dialog = context?.let { it1 -> Dialog(it1) }
-                dialog?.setContentView(R.layout.fireinspector_dialogspinner)
-                dialog?.window?.setLayout(650,800)
-                dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-                dialog?.show()
+            val dialogWidth = WindowManager.LayoutParams.MATCH_PARENT
+            val dialogHeight = WindowManager.LayoutParams.WRAP_CONTENT
+            dialog.window?.setLayout(650,800)
 
-                val recyCleView = dialog?.findViewById<RecyclerView>(R.id.fireInspectorRecycleView)
-                recyCleView?.layoutManager = LinearLayoutManager(context)
-                val adapter = AdapterForFireInspectorList(fireInspectorList)
+            val location = IntArray(2)
+            binding.fireInspector.getLocationOnScreen(location)
+            val x = location[0]-20
+            val y = location[1] + binding.fireInspector.height - 140
 
-                recyCleView?.adapter = adapter
+            dialog.window?.setGravity(Gravity.TOP or Gravity.START)
+            dialog.window?.attributes?.x = x
+            dialog.window?.attributes?.y = y
+
+            val fireInspectorRecyclerView = dialog.findViewById<RecyclerView>(R.id.fireInspectorRecycleView)
+            val fireInspectorSearchView = dialog.findViewById<EditText>(R.id.searchview)
+            fireInspectorRecyclerView.layoutManager = LinearLayoutManager(context)
+            val adapter = AdapterForFireInspectorList(fireInspectorList,binding.fireInspector,dialog)
+            fireInspectorRecyclerView.adapter = adapter
+
+            dialog.show()
+
+
+            fireInspectorSearchView.addTextChangedListener(object: TextWatcher {
+                @SuppressLint("NotifyDataSetChanged")
+                override fun afterTextChanged(s: Editable?) {
+
+                    if(!s.toString().isEmpty()) {
+                        val tempArrayList = ArrayList<String>()
+                        for (value in fireInspectorList) {
+                            Log.d("neel", s.toString())
+                            if (value.toLowerCase(Locale.ROOT).contains(s.toString().toLowerCase(Locale.ROOT).trim())) tempArrayList.add(value)
+                        }
+                        val newAdapter = AdapterForFireInspectorList(tempArrayList,binding.fireInspector,dialog)
+                        fireInspectorRecyclerView.adapter = newAdapter
+                        newAdapter.notifyDataSetChanged()
+                    }else{
+                        val adapter = AdapterForFireInspectorList(fireInspectorList,binding.fireInspector,dialog)
+                        fireInspectorRecyclerView.adapter = adapter
+                        adapter.notifyDataSetChanged()
+                    }
+
+                }
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            })
+
+
         }
+
 
 
         return binding.root
